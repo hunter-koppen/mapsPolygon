d880df5cc5d1ed226c669255d85a0511d459bd7b
Hunter Koppen <hkoppen@clevr.com>
2023-02-12 22:26:53 +0100

    loaded a map

--- src/components/HelloWorldSample.jsx
+++ src/components/HelloWorldSample.jsx
@@ -1,7 +1,25 @@
 import { Component, createElement } from "react";
 
+import GoogleMapReact from "google-map-react";
+
 export class HelloWorldSample extends Component {
+    
     render() {
-        return <div className="widget-hello-world">Hello {this.props.sampleText}</div>;
+        const defaultProps = {
+            center: {
+              lat: 10.99835602,
+              lng: 77.01502627
+            },
+            zoom: 11
+          };
+        return (
+            <div style={{ height: "600px", width: "800px" }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyDV9WEiFQF3VEX1H2Yjg64Pahbv78aaJig" }}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
+                ></GoogleMapReact>
+            </div>
+        );
     }
 }
