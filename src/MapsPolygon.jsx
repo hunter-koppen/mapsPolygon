ce88ed83857d5a73a6776d8cab510b4146f374e6
Hunter Koppen <hunter.koppen@kobeon.nl>
2025-01-20 17:08:18 +0100

    rewrote widget to functional components

--- src/MapsPolygon.jsx
+++ src/MapsPolygon.jsx
@@ -1,45 +1,43 @@
-import { Component, createElement } from "react";
+import { createElement } from "react";
 
 import { MapContainer } from "./components/MapContainer";
 
-export class MapsPolygon extends Component {
-    render() {
-        return (
-            <MapContainer
-                googleKey={this.props.googleKey.value}
-                mapType={this.props.mapType}
-                scrollwheel={this.props.scrollwheel}
-                mapTypeControl={this.props.mapTypeControl}
-                zoomControl={this.props.zoomControl}
-                rotateControl={this.props.rotateControl}
-                fullReload={this.props.fullReload.value}
-                autoZoom={this.props.autoZoom}
-                zoom={this.props.zoom}
-                autoTilt={this.props.autoTilt}
-                tilt={this.props.tilt}
-                panByX={this.props.panByX}
-                panByY={this.props.panByY}
-                dutchImagery={this.props.dutchImagery}
-                width={this.props.width}
-                height={this.props.height}
-                streetViewControl={this.props.streetViewControl}
-                fullscreenControl={this.props.fullscreenControl}
-                polygonList={this.props.polygonList}
-                coordinates={this.props.coordinates}
-                onClickPolygon={this.props.onClickPolygon}
-                fillColor={this.props.fillColor}
-                fillOpacity={this.props.fillOpacity}
-                strokeColor={this.props.strokeColor}
-                strokeOpacity={this.props.strokeOpacity}
-                strokeWeight={this.props.strokeWeight}
-                polygonLabel={this.props.polygonLabel}
-                labelColor={this.props.labelColor}
-                labelSize={this.props.labelSize}
-                labelClass={this.props.labelClass}
-                mapId={this.props.mapId}
-                classNames={this.props.class}
-                caching={this.props.caching}
-            />
-        );
-    }
+export function MapsPolygon(props) {
+    return (
+        <MapContainer
+            googleKey={props.googleKey.value}
+            mapType={props.mapType}
+            scrollwheel={props.scrollwheel}
+            mapTypeControl={props.mapTypeControl}
+            cameraControl={props.cameraControl}
+            rotateControl={props.rotateControl}
+            fullReload={props.fullReload.value}
+            autoZoom={props.autoZoom}
+            zoom={props.zoom}
+            autoTilt={props.autoTilt}
+            tilt={props.tilt}
+            panByX={props.panByX}
+            panByY={props.panByY}
+            dutchImagery={props.dutchImagery}
+            width={props.width}
+            height={props.height}
+            streetViewControl={props.streetViewControl}
+            fullscreenControl={props.fullscreenControl}
+            polygonList={props.polygonList}
+            coordinates={props.coordinates}
+            onClickPolygon={props.onClickPolygon}
+            fillColor={props.fillColor}
+            fillOpacity={props.fillOpacity}
+            strokeColor={props.strokeColor}
+            strokeOpacity={props.strokeOpacity}
+            strokeWeight={props.strokeWeight}
+            polygonLabel={props.polygonLabel}
+            labelColor={props.labelColor}
+            labelSize={props.labelSize}
+            labelClass={props.labelClass}
+            mapId={props.mapId}
+            classNames={props.class}
+            caching={props.caching}
+        />
+    );
 }
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,40 +1,24 @@
-import { Component, createElement } from "react";
+import { createElement, useState, useEffect, useCallback, useRef } from "react";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 import { createPolygon } from "./Polygon";
 import { createLabel } from "./Label";
 import { resizeMap, clearMapItems, createClusterer, clusterMap } from "./Map";
 
-export class MapContainer extends Component {
-    state = {
+export function MapContainer(props) {
+    const [state, setState] = useState({
         loaded: false,
         map: null,
         polygons: [],
         labels: [],
         labelCluster: null,
         clickedPolygon: null
-    };
+    });
+    
+    const prevPolygonListRef = useRef(props.polygonList);
 
-    componentDidUpdate(prevProps) {
-        const { polygonList, fullReload } = this.props;
-        const { loaded, map } = this.state;
-
-        if (!loaded && polygonList?.status === "available" && map) {
-            this.loadData();
-            return;
-        }
-
-        if (prevProps.polygonList.items !== polygonList.items && map) {
-            if (fullReload) {
-                this.loadData();
-            } else {
-                this.updatePolygon();
-            }
-        }
-    }
-
-    loadData = () => {
-        const { map, polygons, labels, labelCluster, loaded } = this.state;
+    const loadData = useCallback(() => {
+        const { map, polygons, labels, labelCluster, loaded } = state;
         const {
             polygonList,
             polygonLabel,
@@ -46,23 +30,21 @@ export class MapContainer extends Component {
             panByX,
             panByY,
             dutchImagery
-        } = this.props;
+        } = props;
         const newPolygons = [];
         const newLabels = [];
 
         const createPolygonWithLabel = mxObject => {
-            const newPolygon = createPolygon(mxObject, google.maps, this.props);
+            const newPolygon = createPolygon(mxObject, google.maps, props);
             if (newPolygon) {
                 if (polygonLabel) {
-                    // if labels have been configured we have to add them for each polygon
-                    const newLabel = createLabel(mxObject, newPolygon, google.maps, this.props);
+                    const newLabel = createLabel(mxObject, newPolygon, google.maps, props);
                     newLabel.setMap(map);
                     newLabels.push(newLabel);
                 }
-                // if an onClickAction has been set we have to add the event for each polygon
                 if (onClickPolygon) {
                     google.maps.event.addListener(newPolygon, "click", event => {
-                        this.handlePolygonClick(onClickPolygon, newPolygon, polygonList);
+                        handlePolygonClick(onClickPolygon, newPolygon, polygonList);
                     });
                 }
                 newPolygon.setMap(map);
@@ -71,25 +53,20 @@ export class MapContainer extends Component {
         };
 
         if (polygonList.items && map) {
-            // Create all the polygons here
             polygonList.items.forEach(createPolygonWithLabel);
             if (loaded) {
-                // when doing a full reload we have to clear all the old polygons and labels
                 clearMapItems(polygons);
                 clearMapItems(labels);
             } else {
-                // this will only run on first load because we need to center on the map
                 resizeMap(newPolygons, google.maps, map, autoZoom, zoom, autoTilt, tilt, panByX, panByY);
-                this.setState({
-                    loaded: true
-                });
+                setState(prev => ({ ...prev, loaded: true }));
             }
 
             if (dutchImagery) {
                 const WMSLayer = new google.maps.ImageMapType({
                     getTileUrl: function (coord, gZoom) {
                         var z2 = Math.pow(2, gZoom);
-                        var tileX = coord.x % z2; // Wrap tiles horizontally.
+                        var tileX = coord.x % z2;
                         if (tileX < 0) tileX += z2;
                         var tileY = coord.y;
 
@@ -106,92 +83,110 @@ export class MapContainer extends Component {
                 map.overlayMapTypes.push(WMSLayer);
             }
 
-            // We then have to add clusters for the labels in case there are too many and they are overlapping
             clusterMap(newLabels, labelCluster);
 
-            // And finally we can set the new polygons and labels to the state.
-            this.setState({
+            setState(prev => ({
+                ...prev,
                 polygons: newPolygons,
                 labels: newLabels
-            });
+            }));
         }
-    };
+    }, [state, props]);
 
-    updatePolygon = () => {
-        const { map, polygons, clickedPolygon } = this.state;
-        const { polygonList, onClickPolygon } = this.props;
+    const updatePolygon = useCallback(() => {
+        const { map, polygons, clickedPolygon } = state;
+        const { polygonList, onClickPolygon } = props;
 
         if (polygonList?.items && map) {
             const clickedObject = polygonList.items.find(poly => poly.id === clickedPolygon.id);
             if (clickedObject) {
                 const newPolygons = polygons;
 
-                // if found first remove the old polygon from the new list
                 const clickedIndex = newPolygons.findIndex(x => x.id === clickedPolygon.id);
                 newPolygons.splice(clickedIndex, 1);
                 clickedPolygon.setMap(null);
 
-                // then create a new polygon based on the new input
-                const newPolygon = createPolygon(clickedObject, google.maps, this.props);
+                const newPolygon = createPolygon(clickedObject, google.maps, props);
 
                 if (newPolygon) {
                     if (onClickPolygon) {
                         google.maps.event.addListener(newPolygon, "click", event => {
-                            this.handlePolygonClick(onClickPolygon, newPolygon, polygonList);
+                            handlePolygonClick(onClickPolygon, newPolygon, polygonList);
                         });
                     }
                     newPolygon.setMap(map);
                     newPolygons.push(newPolygon);
-                    this.setState({
+                    setState(prev => ({
+                        ...prev,
                         polygons: newPolygons
-                    });
+                    }));
                 }
             }
         }
-    };
+    }, [state, props]);
 
-    handlePolygonClick = (onClickPolygon, polygon, polygonList) => {
+    const handlePolygonClick = useCallback((onClickPolygon, polygon, polygonList) => {
         if (onClickPolygon && polygon) {
             const mxObjectClicked = polygonList.items.find(poly => poly.id === polygon.id);
             if (mxObjectClicked) {
                 onClickPolygon.get(mxObjectClicked).execute();
             }
-            this.setState({
+            setState(prev => ({
+                ...prev,
                 clickedPolygon: polygon
-            });
+            }));
         }
-    };
+    }, []);
 
-    handleMapLoad = mapEvent => {
+    const handleMapLoad = useCallback(mapEvent => {
         const map = mapEvent.map;
         const labelCluster = createClusterer(map, google.maps);
-        this.setState({ map, labelCluster });
-    };
+        setState(prev => ({ ...prev, map, labelCluster }));
+    }, []);
 
-    render() {
-        const { height, width, googleKey, classNames } = this.props;
-        const defaultCenter = { lat: 52.383564, lng: 4.645537 };
-        const defaultZoom = 10;
+    useEffect(() => {
+        const { polygonList, fullReload } = props;
+        const { loaded, map } = state;
 
-        return (
-            <div style={{ height, width }} className={"mx-polygonmap " + classNames}>
-                <APIProvider apiKey={googleKey}>
-                    <Map
-                        reuseMaps={this.props.caching}
-                        defaultCenter={defaultCenter}
-                        defaultZoom={defaultZoom}
-                        onIdle={!this.state.loaded ? this.handleMapLoad : null}
-                        mapId={this.props.mapId}
-                        mapTypeId={this.props.mapType}
-                        zoomControl={this.props.zoomControl}
-                        mapTypeControl={this.props.mapTypeControl}
-                        streetViewControl={this.props.streetViewControl}
-                        fullscreenControl={this.props.fullscreenControl}
-                        rotateControl={this.props.rotateControl}
-                        gestureHandling={this.props.scrollwheel ? "greedy" : "none"}
-                    />
-                </APIProvider>
-            </div>
-        );
-    }
+        if (!loaded && polygonList?.status === "available" && map) {
+            loadData();
+            return;
+        }
+
+        if (polygonList.items !== prevPolygonListRef.current?.items && map) {
+            if (fullReload) {
+                loadData();
+            } else {
+                updatePolygon();
+            }
+        }
+
+        prevPolygonListRef.current = polygonList;
+    }, [props.polygonList, props.fullReload, state.loaded, state.map, loadData, updatePolygon]);
+
+    const { height, width, googleKey, classNames } = props;
+    const defaultCenter = { lat: 52.383564, lng: 4.645537 };
+    const defaultZoom = 10;
+
+    return (
+        <div style={{ height, width }} className={"mx-polygonmap " + classNames}>
+            <APIProvider apiKey={googleKey}>
+                <Map
+                    reuseMaps={props.caching}
+                    defaultCenter={defaultCenter}
+                    defaultZoom={defaultZoom}
+                    onIdle={!state.loaded ? handleMapLoad : null}
+                    mapId={props.mapId}
+                    mapTypeId={props.mapType}
+                    disableDefaultUI={true}
+                    mapTypeControl={props.mapTypeControl}
+                    streetViewControl={props.streetViewControl}
+                    fullscreenControl={props.fullscreenControl}
+                    rotateControl={props.rotateControl}
+                    cameraControl={props.cameraControl}
+                    gestureHandling={props.scrollwheel ? "greedy" : "none"}
+                />
+            </APIProvider>
+        </div>
+    );
 }
